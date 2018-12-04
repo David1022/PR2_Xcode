357268398b1eae7deb534d39d7cb0953beeb27d2
David Mendaño <david.mendano@movetia.com>
2018-12-04 15:48:33 +0100

    Ejercicio 5: Añadida table view. Falta formateo de fecha e importe

--- PR2/MovementCell.swift
+++ PR2/MovementCell.swift
@@ -9,9 +9,20 @@ import UIKit
 
 class MovementCell: UITableViewCell {
 // BEGIN-UOC-4
-    @IBOutlet weak var descriptionCell: UIView!
+    @IBOutlet weak var descriptionCell: UILabel!
+    
     @IBOutlet weak var dateCell: UILabel!
+    
     @IBOutlet weak var amountCell: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
     
 // END-UOC-4
 }
--- PR2/MovementsListViewController.swift
+++ PR2/MovementsListViewController.swift
@@ -24,16 +24,45 @@ class MovementsListViewController: UITableViewController {
     
     
     // BEGIN-UOC-5
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 0
+//    }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return movements.count
     }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: MovementCell = tableView.dequeueReusableCell(withIdentifier: "MovementCell", for: indexPath) as! MovementCell
+        let description: String = movements[indexPath.row].movementDescription
+        let date: Date = movements[indexPath.row].date
+        let amount: Decimal = movements[indexPath.row].amount
 
+        
+        cell.descriptionCell.text = description
+        cell.dateCell.text = getFormattedDate(dateToFormat: date)
+        cell.amountCell.text = getFormattedAmount(amountToFormat: amount)
+        
+        if (amount < 0) {
+            cell.amountCell.textColor = UIColor.red
+        } else {
+            cell.amountCell.textColor = UIColor.black
+        }
+        
+        return cell
+    }
+    
+    func getFormattedDate(dateToFormat date: Date) -> String{
+        // TODO Format date
+        return "2018-12-01"
+    }
+    
+    func getFormattedAmount(amountToFormat amount: Decimal) -> String {
+        // TODO Format amount
+        return "\(amount)"
+    }
     // END-UOC-5
     
     // BEGIN-UOC-7
